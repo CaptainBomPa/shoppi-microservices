bd49052d1ebd84d7b84f50dc3e5a3aa06ae0da7e
Filip Mróz <filip.mroz12@gmail.com>
2025-03-02 12:49:35 +0100

    Basic setup of user service.

---
+++ user-service/src/main/java/me/fmroz/shoppi/ShoppiApplication.java
@@ -0,0 +1,13 @@
+package me.fmroz.shoppi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppiApplication.class, args);
+    }
+
+}
---
+++ user-service/src/test/java/me/fmroz/shoppi/ShoppiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.fmroz.shoppi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
