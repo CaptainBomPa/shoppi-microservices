0537e20b169ec4c96fe0a10a56d62fdc84513666
Filip Mróz <filip.mroz12@gmail.com>
2025-03-14 22:19:21 +0100

    architecture: fix prometheus, loki, promtail. Prepared basic datasources and dashboard for grafana

--- api-gateway/src/main/java/me/fmroz/gateway/common/PublicEndpoints.java
+++ api-gateway/src/main/java/me/fmroz/gateway/common/PublicEndpoints.java
@@ -8,6 +8,7 @@ public class PublicEndpoints {
             "/api/v1/users/login",
             "/api/v1/users/refresh-token",
             "/api/v1/actuator/**",
-            "/actuator/gateway/routes"
+            "/actuator/gateway/routes",
+            "/actuator/prometheus"
     };
 }
